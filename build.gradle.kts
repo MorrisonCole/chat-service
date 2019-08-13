d71937823bc9fcf2a4d41e9a454770083225dc76
Morrison Cole <admin@morrisoncole.co.uk>
2019-08-13 12:59:16 +0900

    Set up proto Java code generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     base
     java
+    id("com.google.protobuf") version("0.8.10")
 }
 
 allprojects {
@@ -12,7 +15,19 @@ allprojects {
     }
 }
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.google.protobuf", "protobuf-gradle-plugin", "0.8.10")
+    }
+}
+
 dependencies {
+    implementation("com.google.protobuf", "protobuf-java", "3.9.1")
+
     subprojects.forEach { project ->
         archives(project)
     }
@@ -21,4 +36,23 @@ dependencies {
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("proto")
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.9.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.22.1"
+        }
+    }
 }
